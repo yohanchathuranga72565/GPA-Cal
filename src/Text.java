4c6d863437effe233f9c865605346454e2b55af6
Yohan Chathuranga <yohanchathuranga72565@gmail.com>
2021-11-05 00:42:00 +0530

    All implementation

---
+++ src/Text.java
@@ -0,0 +1,33 @@
+
+import Home.*;
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Text {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Gpa o=new Gpa();
+       // o.setVisible(true);
+       Gpa i=new Gpa();
+       i.setVisible(true);
+        
+  
+    
+    }
+    
+}
